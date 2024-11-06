56bd57ee9ebeab795250695641da3dbf9ca598b1
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 21:14:57 +0530

    index changed

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
  
-<a href="https://safeqloud-228cbc38a2be.herokuapp.com/public/index.php/ApplyNow">Hello from Qloudid.com</a>
+
 <?php 
-die; header('location:http://safeqloud.com/public/index.php/QloudidPersonal');
+header('location:http://safeqloud.com/public/index.php/QloudidPersonal');
 ?>
