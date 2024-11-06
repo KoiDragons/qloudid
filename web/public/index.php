9da9daa1bceec50611b3dc51c6995ce2e0e222b5
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 20:10:20 +0530

    test error

--- web/public/index.php
+++ web/public/index.php
@@ -1,5 +1,7 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
 $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
 $myurl = explode( '/', $uri_parts[0] );
 
@@ -7,7 +9,7 @@ $myurl = explode( '/', $uri_parts[0] );
 //array_shift($myurl);
 //array_shift($myurl);
 //array_shift($myurl);
-print_r($myurl); echo $myurl[3].'Controller.php'; die;
+//print_r($myurl); echo $myurl[3].'Controller.php'; die;
 require_once $myurl[3].'Controller.php';
 
 if(!isset($myurl[2])){
