eb0af617565af53ed0412d4611d6337bc3c298e6
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 06:18:44 +0530

    index file

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
 $myurl = explode( '/', $uri_parts[0] );
 //print_r($myurl); die;
-array_shift($myurl);
-array_shift($myurl);
+//array_shift($myurl);
+//array_shift($myurl);
 array_shift($myurl);
 array_shift($myurl);
  
