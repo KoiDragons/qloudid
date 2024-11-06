9015b12a74151df2f7f2d98912e5d37e79fe7bd7
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 20:18:27 +0530

    appmodel removed

--- web/public/ApplyNowController.php
+++ web/public/ApplyNowController.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'ApplyNowModel.php';
-require_once '../configs/utility.php';
-require_once('../AppModel.php');
+//require_once '../configs/utility.php';
+//require_once('../AppModel.php');
 class ApplyNowController
 {
     
