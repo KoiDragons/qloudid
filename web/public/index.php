ed5ebfc803df798257f202b96a19809bb40755c7
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 19:54:11 +0530

    controller

--- web/public/index.php
+++ web/public/index.php
@@ -4,7 +4,7 @@ $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
 $myurl = explode( '/', $uri_parts[0] );
 
 //array_shift($myurl);
-//array_shift($myurl);
+array_shift($myurl);
 array_shift($myurl);
 array_shift($myurl);
  echo $myurl[3].'Controller.php'; die;
