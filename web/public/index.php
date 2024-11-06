e6aaea05e03526169d291d008afab0e8cf58f9cf
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 19:42:12 +0530

    change url

--- web/public/index.php
+++ web/public/index.php
@@ -2,12 +2,12 @@
 
 $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
 $myurl = explode( '/', $uri_parts[0] );
-print_r($myurl); die;
+
+//array_shift($myurl);
 //array_shift($myurl);
 array_shift($myurl);
 array_shift($myurl);
-array_shift($myurl);
- 
+ print_r($myurl[1]); die;
 require_once $myurl[1].'Controller.php';
 
 if(!isset($myurl[2])){
