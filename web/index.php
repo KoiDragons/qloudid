d96f391cf938f4c1c28aea25b3959f514e4cf500
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 19:35:44 +0530

    data display

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
-
+ 
 <a href="https://safeqloud-228cbc38a2be.herokuapp.com/public/index.php/ApplyNow">Hello from Qloudid.com</a>
 <?php 
-header('location:http://safeqloud.com/public/index.php/QloudidPersonal');
+die; header('location:http://safeqloud.com/public/index.php/QloudidPersonal');
 ?>
