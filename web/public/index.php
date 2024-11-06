0eab2b407d91dc9b1b4cfe44638fb2ef0e4b3a2f
Ankita Jain <kowaken.ghirmai@gmail.com>
2024-11-06 20:37:39 +0530

    array slice

--- web/public/index.php
+++ web/public/index.php
@@ -15,7 +15,7 @@ require_once $myurl[3].'Controller.php';
 if(!isset($myurl[2])){
 	$myurl[2] = "index";
 }
- 
-call_user_func_array(array($myurl[3].'Controller' , $myurl[2] )) ;
+ print_r( array_slice($myurl, 1)); die;
+call_user_func_array(array($myurl[3].'Controller' , $myurl[2] ), array_slice($myurl, 1) ) ;
 
 ?>
